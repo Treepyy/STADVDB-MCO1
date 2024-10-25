e1b0184281b034d1e6639bfa307e69273f127980
Treepyy <105573576+Treepyy@users.noreply.github.com>
2024-10-25 23:29:07 +0800

    update ports for deployed db

--- pages/api/roll-up.ts
+++ pages/api/roll-up.ts
@@ -18,6 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const connection = await mysql.createConnection({
       host: process.env.DB_HOST,
+      port: 21345,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
